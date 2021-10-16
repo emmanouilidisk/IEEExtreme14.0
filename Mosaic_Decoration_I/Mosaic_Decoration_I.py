36003c181c4069b20cb0eecd0c9c8f068d749343
Stathis Karypidis <45715823+Sta8is@users.noreply.github.com>
2021-10-16 20:11:30 +0300

    Update Mosaic_Decoration_I.py

--- Mosaic_Decoration_I/Mosaic_Decoration_I.py
+++ Mosaic_Decoration_I/Mosaic_Decoration_I.py
@@ -1,22 +1,18 @@
 import math
 inp = list(map(int,input().split()))
-N_rows = inp[0]
-M_col = inp[1]
-R_rows = inp[2]
-C_col = inp[3]
-costs = []
-for i in range(R_rows):
-    costs.append(input().split())
-# print(costs)'
-a = math.ceil(N_rows/R_rows)
-b = math.ceil(M_col/C_col)
-if (a*R_rows)%N_rows != 0:
-    pass
-if (b*C_col)%M_col !=0:
-    pass
-R_C_cost = 0
-tiles = a*b
-for i in costs:
-    for j in i:
-        R_C_cost+=int(j)
-print(a*b*R_C_cost)
+ts = inp[0]
+cb = inp[1]
+cp = inp[2]
+# print(ts)
+# print(cb)
+res = 0
+bt = 0
+pt = 0
+for i in range(ts):
+    tiles = list(map(int,input().split()))
+    bt  = bt + tiles[0]
+    pt  = pt + tiles[1]
+tbt = math.ceil(bt/10)
+tpt = math.ceil(pt/10)
+print(tbt*cb+tpt*cp)
+    
